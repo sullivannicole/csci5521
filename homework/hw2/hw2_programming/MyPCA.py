2390722fa6da260a5749672efa0f6893d993cecf
Nicole Sullivan <nicolesullivan@Nicoles-MacBook-Pro-2.local>
2022-02-26 12:55:23 -0600

    finish hw2 programming assignment

--- homework/hw2/hw2_programming/MyPCA.py
+++ homework/hw2/hw2_programming/MyPCA.py
@@ -8,28 +8,44 @@ class PCA():
 
     def fit(self,X):
         # normalize the data to make it centered at zero (also store the means as class attribute)
+        self.mean = np.mean(X)
+        X_norm = X - self.mean
+
 
         # finding the projection matrix that maximize the variance (Hint: for eigen computation, use numpy.eigh instead of numpy.eig)
-
+        X_cov = np.cov(X_norm, ddof = 1)
+        evalues, evectors = np.linalg.eigh(X_cov)
+        
+        evalues_sorted = np.sort(evalues)[::-1]
+        evectors_sorted = np.sort(evectors)[::-1]
+        
         if self.num_dim is None:
-            # select the reduced dimension that keep >90% of the variance
+            # select the reduced dimension that keeps >90% of the variance
+            num_lambdas = 0
+            PoV = 0
+            
+            while PoV < 0.9:
+                PoV += evalues_sorted[num_lambdas]/np.sum(evalues_sorted)
+                num_lambdas += 1
 
             # store the projected dimension
-            self.num_dim = 784 # placeholder
+            self.num_dim = num_lambdas
 
         # determine the projection matrix and store it as class attribute
-        self.W = None # placeholder
+        self.W = evectors_sorted[:self.num_dim]
 
         # project the high-dimensional data to low-dimensional one
-        X_pca = X # placeholder
+        #X_pca = X # placeholder
+        X_pca = self.mean + evalues_sorted[:self.num_dim] @ self.W
 
         return X_pca, self.num_dim
 
     def predict(self,X):
         # normalize the test data based on training statistics
-
+        X_norm = X - self.mean
+        
         # project the test data
-        X_pca = X # placeholder
+        X_pca = X_norm @ self.W
 
         return X_pca
 
--- homework/hw2/hw2_programming/sandbox.py
+++ homework/hw2/hw2_programming/sandbox.py
@@ -9,6 +9,7 @@ ytrain=data[data[:,0]!=5,1]
 Xtest=data[data[:,0]==5,2:]
 ytest=data[data[:,0]==5,1]
 
+#----------for k means
 
 init_idx = [1, 200, 500, 1000, 1001, 1500, 2000, 2005] #intial indices
 centers = Xtrain[init_idx]
@@ -29,3 +30,39 @@ centers[0] = np.mean(Xtrain[np.where(cluster_assignment == i)], axis = 0)
 for i in range(len(centers)):
     centers[i] = np.mean(Xtrain[np.where(cluster_assignment == i+1)], axis = 0)
 
+
+#-------for PCA
+
+num_dim = None
+mean = np.zeros([1,784]) # means of training data
+W = None # projection matrix
+
+X_norm = Xtrain - np.mean(Xtrain)
+
+X_cov = np.cov(X_norm, ddof = 1)
+
+evalues, evectors = np.linalg.eigh(X_cov)
+
+# Sort largest to smallest
+# Per docs, both vals and vector come out asc, so reversing the order ensures they're still matched
+evalues_sorted = np.sort(evalues)[::-1]
+
+evectors_sorted = np.sort(evectors)[::-1]
+
+# Find # of PCs needed to capture > 90% of variance
+num_lambdas = 0
+PoV = 0
+
+while PoV < 0.9:
+    PoV += evalues_sorted[num_lambdas]/np.sum(evalues_sorted)
+    num_lambdas += 1
+
+W = evectors_sorted[0:(num_lambdas - 1)]
+
+# Get matching indices for matching evectors
+#evector_indices = np.where(np.in1d(X_evalues, X_evalues_sorted[0:(num_lambdas-1)]))[0]
+
+
+W = X_evectors[evector_indices]
+    
+    
